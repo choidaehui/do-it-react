12e418d9583ecbac5991b29478bae788cbe7486b
DaeHui <77823790+choidaehui@users.noreply.github.com>
2021-08-22 13:44:33 +0900

    react: 클래스형 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,55 @@
-import React, { Component } from 'react';
-import BooleanComponent from './component/BooleanComponent';
-import ChildComponent2 from './component/ChildComponent2';
-import Counter from './component/Counter';
-import LifecycleExample from './component/LifecycleExample';
-import NewCounter from './component/NewCounter';
-import PrevCounter from './component/PrevCounter';
-import StateExample from './component/StateExample';
+import React from 'react';
 
 // 작성된 컴포넌트를 App.js로 불러오기
-// import 'MyComponent';의 경우 MyComponent.js > MyComponent.jsx > 파일이 없으면 MyComponent 폴더를 검색 
+// import 'MyComponent';의 경우 MyComponent.js > MyComponent.jsx > 파일이 없으면 MyComponent 폴더를 검색
 // 해당 폴더 안에 index.js > index.jsx 순서로 파일을 확인하여 임포트 함
-import TodaysPlan from './component/TodaysPlan';
+
+class MyComponent extends React.Component {
+  componentDidUpdate() {
+    console.log('MyComponent 새로 고침');
+  }
+}
+
+class MyPureComponent extends React.PureComponent {
+  componentDidUpdate() {
+    console.log('MyPureComponent 새로 고침');
+  }
+}
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { count: 10 };
-    this.resetCount = this.resetCount.bind(this);
+    this.listValue = [{ name: 'Park' }, { name: 'Lee' }];
+    this.state = { version: 0 };
+    this.handleClick = this.handleClick.bind(this);
   }
-  resetCount() {
-    this.setState(({ count }) => ({ count: count + 10 }));
+
+  handleClick() {
+    setTimeout(() => {
+      this.listValue[0].name = 'Justin';
+      this.setState({ version: 1 });
+    }, 100)
+    setTimeout(() => {
+      this.listValue = [{ name: 'Justin' }, { name: 'Lee' }];
+      this.setState({ version: 2 });
+    }, 200);
   }
+
   render() {
     return (
-       // JSX는 HTML이 아니라 XML 마크업 규칙을 따른다.
-       // 엘리먼트의 시작표시와 < 마침 표시의 />의 짝이 맞아야 한다.
-       // 컴포넌트를 JSX 안에 마크업 형식으로 추가
-      <div>
-        <div><PrevCounter count={this.state.count} /></div>
-        <div><NewCounter count={this.state.count} /></div>
-        <button onClick={this.resetCount}>{this.state.count + 10}으로 초기화</button>
+      // JSX는 HTML이 아니라 XML 마크업 규칙을 따른다.
+      // 엘리먼트의 시작표시와 < 마침 표시의 />의 짝이 맞아야 한다.
+      // 컴포넌트를 JSX 안에 마크업 형식으로 추가
+      <div className="body">
+        <MyComponent value={this.listValue} />
+        <MyPureComponent value={this.listValue} />
+        <button onClick={this.handleClick}>버튼</button>
       </div>
     );
   }
 }
 
-export default App;
+export default App
 
 // 컴포넌트 구성 요소
 // 1. 프로퍼티: 상위 컴포넌트에서 하위 컴포넌트로 전달되는 읽기 전용 데이터
